f99b0024a410113407960628366b75851a38ba21
0kihS <teampokemonleet@gmail.com>
2024-01-16 20:04:22 +0000

    add tests

--- commands/ranked/enteruser.js
+++ commands/ranked/enteruser.js
@@ -25,14 +25,14 @@ module.exports = {
     // Check if the user with the same name already exists in the ladder
     const result = await client.query('SELECT COUNT(*) FROM players WHERE name = $1;', [name]);
 
-    if (!result.rows[0].count > 0) {
-
-    //Insert new user
-    const result = await client.query('INSERT INTO players (name, elo, wins) VALUES ($1, 1000, 0) RETURNING *;', [name]);
-    await interaction.reply(`Added <@${result.rows[0].name}> to the ranked leaderboard!.`);
-
-    } else {
+    if (result.rows[0].count > 0) {
       await interaction.reply(`User <@${user.id}> is already registered!.`);
+   
+    } 
+    else {
+      const result = await client.query('INSERT INTO players (name, elo, wins) VALUES ($1, 1000, 0) RETURNING *;', [name]);
+      await interaction.reply(`Added <@${result.rows[0].name}> to the ranked leaderboard!.`);
+  
     }
   } catch (error) {
     console.log('Error:', error);
